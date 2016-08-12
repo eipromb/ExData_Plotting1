3132e210ddf3365c475224799adec2e86744bfdb
eipromb <jason@makepigeonpie.com>
2016-08-12 15:58:18 -0400

    Assignment submission

---
+++ plot2.R
@@ -0,0 +1,39 @@
+# required rows
+first_row <- 66638
+last_row <- 69517
+# column names
+column_names <- c('Date',
+                  'Time',
+                  'Global_active_power',
+                  'Global_reactive_power',
+                  'Voltage',
+                  'Global_intensity',
+                  'Sub_metering_1',
+                  'Sub_metering_2',
+                  'Sub_metering_3')
+# column classes
+column_classes <- c('character',
+                    'character',
+                    'numeric',
+                    'numeric',
+                    'numeric',
+                    'numeric',
+                    'numeric',
+                    'numeric',
+                    'numeric')
+
+# read the table
+household_pwr <- read.table('household_power_consumption.txt', 
+                            sep=';', 
+                            skip = first_row - 1,
+                            nrows = last_row - first_row + 1,
+                            col.name=column_names, 
+                            colClasses = column_classes)
+
+# fix date/times
+household_pwr$datetime <- strptime(paste(household_pwr$Date, household_pwr$Time), format='%d/%m/%Y %H:%M:%S')
+
+# plot
+png('plot2.png', height=480, width=480)
+plot(household_pwr$datetime, household_pwr$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,49 @@
+# required rows
+first_row <- 66638
+last_row <- 69517
+# column names
+column_names <- c('Date',
+                  'Time',
+                  'Global_active_power',
+                  'Global_reactive_power',
+                  'Voltage',
+                  'Global_intensity',
+                  'Sub_metering_1',
+                  'Sub_metering_2',
+                  'Sub_metering_3')
+# column classes
+column_classes <- c('character',
+                    'character',
+                    'numeric',
+                    'numeric',
+                    'numeric',
+                    'numeric',
+                    'numeric',
+                    'numeric',
+                    'numeric')
+
+# read the table
+household_pwr <- read.table('household_power_consumption.txt', 
+                            sep=';', 
+                            skip = first_row - 1,
+                            nrows = last_row - first_row + 1,
+                            col.name=column_names, 
+                            colClasses = column_classes)
+
+# fix date/times
+household_pwr$datetime <- strptime(paste(household_pwr$Date, household_pwr$Time), format='%d/%m/%Y %H:%M:%S')
+
+# plot
+png('plot4.png', height=480, width=480)
+par(mfrow=c(2,2))
+
+with(household_pwr, plot(datetime, Global_active_power, type="l", xlab="", ylab="Global Active Power"))
+with(household_pwr, plot(datetime, Voltage, type="l"))
+
+with(household_pwr, plot(datetime, Sub_metering_1, type="l", col="black", ylab="Energy sub metering", xlab=""))
+with(household_pwr, lines(datetime, Sub_metering_2, col="red"))
+with(household_pwr, lines(datetime, Sub_metering_3, col="blue"))
+legend("topright", col=c("black","red","blue"), lwd=1, bty='n', legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+with(household_pwr, plot(datetime, Global_reactive_power, type="l"))
+dev.off()
